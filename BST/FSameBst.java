ef0c81cbc1d1dafc4401fff562634709f953bb94
Billionare <70501837+ShivomSehgal@users.noreply.github.com>
2021-05-20 17:09:01 +0530

    Update FSameBst.java

--- BST/FSameBst.java
+++ BST/FSameBst.java
@@ -2,6 +2,48 @@ import java.util.*;
 
 class Program {
   public static boolean sameBsts(List<Integer> arrayOne, List<Integer> arrayTwo) {
+    if(arrayOne.size() != arrayTwo.size())return false;
+		
+		if(arrayOne.size() == 0 && arrayTwo.size()==0)return true;
+		
+		if(arrayOne.get(0).intValue() != arrayTwo.get(0).intValue())return false;
+		
+		List<Integer> leftOne=getSmaller(arrayOne);
+		List<Integer> leftTwo=getSmaller(arrayTwo);
+		List<Integer> rightOne=getBigger(arrayOne);
+		List<Integer> rightTwo=getBigger(arrayTwo);
+		
+		return sameBsts(leftOne,leftTwo) && sameBsts(rightOne,rightTwo);
+  }
+	
+	public static List<Integer> getSmaller(List<Integer> array){
+		List<Integer> smaller=new ArrayList<>();
+		for(int i=1; i<array.size();i++){
+			if(array.get(0).intValue()>array.get(i).intValue()){
+				smaller.add(array.get(i));
+			}
+		}
+		return smaller;
+	}
+	public static List<Integer> getBigger(List<Integer> array){
+		List<Integer> smaller=new ArrayList<>();
+		for(int i=1; i<array.size();i++){
+			if(array.get(0).intValue() <= array.get(i).intValue()){
+				smaller.add(array.get(i));
+			}
+		}
+		return smaller;
+	}
+}
+
+
+
+
+
+import java.util.*;
+
+class Program {
+  public static boolean sameBsts(List<Integer> arrayOne, List<Integer> arrayTwo) {
     
 		Map<Integer,Integer> map=new HashMap<>();
 		
