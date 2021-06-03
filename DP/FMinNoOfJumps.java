5b972a93a23234b72892abb3123c0e96c4bfb141
Billionare <70501837+ShivomSehgal@users.noreply.github.com>
2021-06-03 17:29:48 +0530

    Create FMinNoOfJumps.java

---
+++ DP/FMinNoOfJumps.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Program {
+  public static int minNumberOfJumps(int[] array) {
+   
+		if(array.length ==1 ){
+			return 0;
+		}
+		
+		int jumps = 0, maxReach=array[0], steps=array[0];
+				
+		for(int i=1; i < array.length-1; i++){
+		 
+			maxReach=Math.max(maxReach,i+array[i]);
+			steps--;
+			if(steps==0){
+				jumps++;
+				steps=maxReach-i;
+				
+			}
+		
+		}
+			return jumps+1;
+
+	}
+}
