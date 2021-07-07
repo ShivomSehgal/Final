08c1492841a346d6f5d58cbfc281141939b410b7
Billionare <70501837+ShivomSehgal@users.noreply.github.com>
2021-07-07 16:24:47 +0530

    Update FKthLargestNode.java

--- BST/FKthLargestNode.java
+++ BST/FKthLargestNode.java
@@ -1,4 +1,55 @@
 import java.util.*;
+  O(h+k) | O(h)
+class Program {
+  
+  static class BST {
+    public int value;
+    public BST left = null;
+    public BST right = null;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+	
+	static class TreeInfo{
+		int num;
+		int val;
+		
+		public TreeInfo(int num, int val){
+			this.num=num;
+			this.val=val;
+		}
+	}
+
+  public int findKthLargestValueInBst(BST tree, int k) {
+    TreeInfo treeInfo= new TreeInfo(0,-1);
+		reverseInOrderTraverse(tree,k,treeInfo);
+    return treeInfo.val;
+  }
+	
+	public void reverseInOrderTraverse(BST tree, int k, TreeInfo treeInfo){
+		if(tree == null || treeInfo.num >= k){
+			return;
+		}
+		reverseInOrderTraverse(tree.right,k,treeInfo);
+		if(treeInfo.num < k){
+			treeInfo.num +=1;
+			treeInfo.val=tree.value;
+			reverseInOrderTraverse(tree.left,k,treeInfo);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+import java.util.*;
 
 class Program {
   
