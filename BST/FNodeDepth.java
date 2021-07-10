aab40004dc816ea1616565aae17b356938b11e7c
Billionare <70501837+ShivomSehgal@users.noreply.github.com>
2021-07-10 15:26:53 +0530

    Update FNodeDepth.java

--- BST/FNodeDepth.java
+++ BST/FNodeDepth.java
@@ -1,6 +1,40 @@
 import java.util.*;
 
 class Program {
+	
+	static class Level{
+		public BinaryTree node;
+		int depth;
+		
+		public Level(BinaryTree node, int depth){
+			this.node=node;
+			this.depth=depth;
+		}
+	}
+	
+  public static int nodeDepths(BinaryTree root) {
+    int sumOfDepths=0;
+		List<Level> array= new ArrayList<Level>();
+		array.add(new Level(root,0));
+		while(array.size() > 0){
+			Level top=array.remove(array.size()-1);
+			BinaryTree tree= top.node;
+			int depth=top.depth;
+			if(tree == null) continue;
+			sumOfDepths += depth;
+			array.add(new Level(tree.left, depth + 1));
+			array.add(new Level(tree.right, depth + 1));
+		}
+    return sumOfDepths;
+  }
+}
+
+
+
+
+import java.util.*;
+
+class Program {
 
   public static int nodeDepths(BinaryTree root) {
     return nodeDepths(root,0);
